06fa88817bb5ae7f9e89b04d6b6bad5f75fd4334
MJotaCM <macoelho@tsn.at>
2022-09-21 10:54:43 +0200

    teil 1 der aufgabe kw 38

---
+++ aufgabe_kw_38.py
@@ -0,0 +1,13 @@
+#Aufgabe Programmiere Lottoziehung als Methode:
+
+# Alternetive Lösung
+"""
+import random
+import numpy as np
+
+all = np.arange(1, 45)
+random.shuffle(all)
+wn = all[0:6]
+
+print(wn)
+"""
