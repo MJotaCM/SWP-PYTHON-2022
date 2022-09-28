beafdfdd2f63226d4109da79bbb673a645ba6afd
MJotaCM <macoelho@tsn.at>
2022-09-28 09:02:26 +0200

    Aufgabe kw 38

--- aufgabe_kw_38.py
+++ aufgabe_kw_38.py
@@ -3,12 +3,20 @@ import numpy as np
 
 #Aufgabe Programmiere Lottoziehung als Methode:
 #Aufgabe 1 
-all = np.arange(1, 46)
-random.shuffle(all)
-print(all[0:6])
+def zahlen_ziehen(start, end, anz):
+    all = np.arange(start, end+1)
+    random.shuffle(all)
+    return all[0:anz]
+
+# Swap-Funktion
+# list[0] , list[1] = list[1], list[0]
 
 #Aufgabe 2
-md = dict.fromkeys(np.arange(1, 46), 0)
-for i in range(0, 1000):
-    md[int(random.random()*45 + 1)] += 1
-print(md)
+def statistik(start, end, anz):
+    d = dict.fromkeys(np.arange(start, end+1), 0)
+    for i in range(anz):
+        d[int(random.random()*end + 1)] += 1
+    return d
+
+print(zahlen_ziehen(1,45,6))
+print(statistik(1,45,1000))
